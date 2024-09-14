3c5814752595d69d837dcbc7d6857683f43f7d2a
鼠尾草 <13562817+liushengyuan123@user.noreply.gitee.com>
2024-09-14 13:37:47 +0800

    error:make an error commit

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -1,23 +1,17 @@
 module.exports = {
-  parserPreset: "conventional-changelog-conventionalcommits",
+  parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
-    "body-leading-blank": [1, "always"],
-    "body-max-line-length": [2, "always", 100],
-    "footer-leading-blank": [1, "always"],
-    "footer-max-line-length": [2, "always", 100],
-    "header-max-length": [2, "always", 100],
-    "scope-case": [2, "always", "lower-case"],
-    "subject-case": [0],
-    "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "type-case": [2, "always", "lower-case"],
-    "type-empty": [2, "never"],
-    "type-enum": [
-      2,
-      "always",
-      ["feat", "fix", "docs", "style", "test", "refactor", "chore", "revert"],
-    ],
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [0],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert']],
   },
 };
-
-console.log("commitlint-config执行了!!");
